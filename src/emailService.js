69349f092068064ea847aee181f2fcb7c33c300f
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-24 18:27:40 -0400

    Changed to gmail smtp

--- src/emailService.js
+++ src/emailService.js
@@ -1,19 +1,20 @@
 const nodemailer = require('nodemailer');
 
 let transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: 'smtp.gmail.com',
+    port: 465,
+    // service: 'gmail',
     auth: {
-        user: '5cca5f002ffa14',
-        pass: 'd6e9fe7c8f6d0e'
+        user: 'majid.dev.shockoohi@gmail.com',
+        pass: 'ThisIsFake123'
     }
 });
 
 const message = {
-    from: 'majid@company.co', // Sender address
-    to: 'customer@email.com',         // List of recipients
+    from: 'majid.dev.shockoohi@gmail.com', // Sender address
+    to: 'majid.shockoohi@gmail.com',         // List of recipients
     subject: 'Testing nodemailer', // Subject line
-    html: '<h1>Nice module you are using</h1><p>Thank you for using ou account</p>' // Plain text body
+    html: '<h1>Nice module you are using</h1><p>Thank you for using this account</p>' // Plain text body
 };
 
 
